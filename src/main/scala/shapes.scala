bcc7c26dbe521aa1ccc2e657e77dcae006d3b79d
Walter Mikula <146771447+walterMikula@users.noreply.github.com>
2025-10-20 15:25:02 +0000

    added the correct implementations of bounding box and shapes. All of the tets pass now b ut there are warnings

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -3,5 +3,7 @@ package edu.luc.cs.laufer.cs371.shapes
 /** data Shape = Rectangle(w, h) | Location(x, y, Shape) */
 enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
+  case Ellipse(rx: Int, ry: Int)
   case Location(x: Int, y: Int, shape: Shape)
+  case Group(shapes: Shape*)
   // TODO add missing cases (see test fixtures)
